b300f4afdf967e3da4927d588ff3d30ea018f919
trsm <takumi.terashima711@gmail.com>
2022-07-09 18:40:36 +0900

    second-commit

--- exam-js-2.js
+++ exam-js-2.js
@@ -1,5 +1,5 @@
 function leapYear(year) {
-  return (year % 4 === 0 && year % 100 !== 0) || year % 400 == 0;
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
 console.log(leapYear(2020));
